4ba9cae54ca2f5a38290a8fca3fc39b057d035d1
Francisco Moro <impropio2@gmail.com>
2017-07-11 15:02:49 +0200

    [FMC]

--- kazoku-web/src/main/java/com/francisco/kazoku/web/controllers/Inicio.java
+++ kazoku-web/src/main/java/com/francisco/kazoku/web/controllers/Inicio.java
@@ -2,13 +2,13 @@ package com.francisco.kazoku.web.controllers;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.francisco.kazoku.comunicacion.dto.PrediccionClima;
 import com.francisco.kazoku.comunicacion.impl.PrediccionClimaImpl;
 import com.francisco.kazoku.comunicacion.interfaces.PrediccionClimaI;
 
@@ -19,9 +19,12 @@ public class Inicio{
     @Value("${app.web.inicio}")
     private String webInicio;
     
+    
+    PrediccionClimaI a;
+    
     @RequestMapping
     public ModelAndView masterLogin(final HttpSession session, final ModelMap model){
-        PrediccionClimaImpl a = new PrediccionClimaImpl();
+        a = new PrediccionClimaImpl();
         a.getPredicciones();
         return new ModelAndView(webInicio);
     }
